1b945097082bbdb36bc2e41706b19fa83a826586
Pedro <pedrotestagabardo@gmail.com>
2026-03-23 17:15:30 -0300

    0323

---
+++ jogos.php
@@ -0,0 +1,11 @@
+<?php 
+
+require('carregar_twig.php');
+require('carregar_pdo.php');
+
+$jogos = $pdo->query('SELECT * FROM jogos');
+$todosjogos = $jogos->fetchAll(PDO::FETCH_ASSOC);
+
+echo $twig->render('jogos.html', [
+    'jogos' => $todosjogos,
+]);
